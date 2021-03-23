d37756a48d59d83313c9c43eea8984d0a2e50e6e
jagg01 <alexgg0197@gmail.com>
2021-03-24 00:05:04 +0100

    Read_thres module ready

--- src/mod_num/read_thres.py
+++ src/mod_num/read_thres.py
@@ -4,20 +4,22 @@ import pandas as pd
 
 def rd_file(filedir,filename,threshold):
     "Add an explanation here, variables and what it does"
-    no_fl  = len(filename)
-    path_f = filedir + filename[0]
-    ptemp  = pd.read_csv(path_f, '\s+', header=None,skiprows=1)
-    time   = ptemp[0].values
-    expec  = zeros((no_fl,len(time)))
-    for i in range(no_fl):
-        name = filedir + filename[i]
-        ptemp = pd.read_csv(name, '\s+', header=None,skiprows=7)
-        expec[i] = ptemp[1].values
+    path_f = filedir + filename
+    ptemp1 = pd.read_csv(path_f, '\s+', header=None,skiprows=1)
+    fl     = ptemp1[0].values
+    no_fl  = len(fl)
 
-    useless=(filename < threshold).all(axis=1)
+    cl     = open(path_f, "r").readlines()
+    line   = cl[1].split()
+    no_cl  = len(line)
+
+    fl_exp = zeros((no_cl,no_fl))
+    for i in range(no_cl):
+        ptemp2 = pd.read_csv(path_f, '\s+', header=None,skiprows=1)
+        fl_exp[i] = ptemp2[i].values
 
     for i in range(len(useless)):
         if useless[i]==True:
-            filename=np.delete(filename,i)
+            del_fl_exp=np.delete(fl_exp,i,0)
 
-    return  expec
+    return fl_exp , del_fl_exp
