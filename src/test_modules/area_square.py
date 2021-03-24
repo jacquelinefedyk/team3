5509f518c93529df8f17c08c5854dcdb93dc25c3
jacquelinefedyk <jacqueline.fedyk@pci.uni-heidelberg.de>
2021-03-24 17:32:15 +0100

    I created a very creative module:)

---
+++ src/test_modules/area_square.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def area_squa(l):
+    """Calculates the area of a square with given side length l.
+
+    :Input: Side length of the square l (float, >=0)
+    :Returns: Area of the square A (float)."""
+
+    if l < 0:
+        raise ValueError("The side length must be >= 0.")
+
+    A = l**2
+    return A
