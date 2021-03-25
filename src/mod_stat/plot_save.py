a5c56b964ddb16804c0ebdd5d77c2de5dc1c1e21
jacquelinefedyk <jacqueline.fedyk@pci.uni-heidelberg.de>
2021-03-25 13:53:10 +0100

    deleted

--- src/mod_stat/plot_save.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-def plot_save(xval_col_name,dataframe,path):
-    plot = sns.pairplot(dataframe,x_vars=xval_col_name,height = 5)
-    save = plt.savefig(os.path.join(path,str(dataframe)+'.png'))
-    return plot, save
