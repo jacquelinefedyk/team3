4ad3c312dd400f2a446db0f553657234809f32ca
jacquelinefedyk <jacqueline.fedyk@pci.uni-heidelberg.de>
2021-03-29 16:35:45 +0200

    additional description is added

--- src/mod_stat_num/stat_corr.py
+++ src/mod_stat_num/stat_corr.py
@@ -2,6 +2,17 @@ import numpy as np
 import pandas as pd
 
 def corr_matrix(dataframe, xval_col_name):
+    """Creates a correlation matrix of the given data frame and orders it by the
+    degree of correlation.
+
+    Args:
+    dataframe (pandas dataframe): Give the data frame
+    xval_col_name (string): Name of the column containing the x-values. This
+    column is deleted.
+
+    Returns:
+    dataframe_corr (pandas series): List of correlations sorted by the degree of correlatin 
+    starting with the highest."""
     # get rid of time column, lower triangular and diagonal entries of the correlation matrix
     dataframe = dataframe.drop(columns=[xval_col_name])
     dataframe_corr = dataframe.corr()
