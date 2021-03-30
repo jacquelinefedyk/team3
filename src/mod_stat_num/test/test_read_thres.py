210f6c9bb4c24e3d0a4af2b77e92a4f4fd87ea27
jagg01 <alexgg0197@gmail.com>
2021-03-30 18:40:06 +0200

    11th try

--- src/mod_stat_num/test/test_read_thres.py
+++ src/mod_stat_num/test/test_read_thres.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
-import read_thres as thrs
+from mymodules import read_thres as thrs
 
 def test_thr():
-    path_f1 ='./test/test_file.csv'
+    path_f1 ='/home/gomezj/PhD/2021_Art_SSD/team3/src/mod_stat_num/test/test_file.csv'
     pt1 = pd.read_csv(path_f1, r'\s+', header=None,skiprows=1)
     fl1 = pt1[0].values
     no_fl1 = len(fl1)
@@ -13,13 +13,15 @@ def test_thr():
     no_cl1 = len(line1)
 
     fl_try = np.zeros((no_cl1,no_fl1))
+    data = np.zeros((no_cl1,no_fl1))
+
     for i in range(no_cl1):
         pt2 = pd.read_csv(path_f1, r'\s+', header=None,skiprows=1)
         fl_try[i] = pt2[i].values
 
-    data, x = thrs.ths_def(fl_try, threshd=1.E-4)
+    filen = 'test_file.csv'
+    filed = '/home/gomezj/PhD/2021_Art_SSD/team3/src/mod_stat_num/test/'
+    thrs= '1.0E-4'
+    data = thrs.ths_def(fl_try, thrs ,filed,filen)
     dat_nw = fl_try.drop(columns=["norm", "<x>", "<y>"])
-    x_nw = dat_nw.columns.values
-    assert len(x) == len(x_nw)
-    assert np.array_equal(x, x_nw)
     assert data.equals(dat_nw)
