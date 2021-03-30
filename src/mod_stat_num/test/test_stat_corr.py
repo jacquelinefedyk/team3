0eada64eec2debceba6950650e9f9ea8c92fa6f8
jacquelinefedyk <jacqueline.fedyk@pci.uni-heidelberg.de>
2021-03-30 17:15:28 +0200

    changes

--- src/mod_stat_num/test/test_stat_corr.py
+++ src/mod_stat_num/test/test_stat_corr.py
@@ -1,3 +1,34 @@
 import pytest
+import pandas as pd
 import numpy as np
 import stat_corr as sc
+
+
+class build_df:
+    def __init__(self, name):
+        self.dfnamepath = name
+
+    def read_df(self):
+        self.data = pd.read_csv(self.dfnamepath, r'\s+')
+        return self.data
+
+
+@pytest.fixture(scope='module')
+def get_df():
+    obj = build_df('./test/test_df.csv')
+    data = obj.read_df()
+    return data
+
+
+
+
+def test_corr_matrix(get_df, xval_col_name):
+    corrmat = sc.corr_matrix(get_df, xval_col_name)
+    corrmat = corrmat.dropna().round(decimals=6)
+    d = [0.832628, 0.228204, 0.136257]
+    ind = pd.MultiIndex.from_tuples([
+                                   ('norm', '<H>'),
+                                   ('<z>', '<H>'),
+                                   ('norm', '<z>')])
+    ser_new = pd.Series(data=d, index=ind)
+    assert corrmat.equals(ser_new)
