f751ceb93e0a98b2c6d41283ec2bbaa3165f02b8
jagg01 <alexgg0197@gmail.com>
2021-03-30 11:36:41 +0200

    Unit 4 changes

--- src/mod_stat_num/num_corr.py
+++ src/mod_stat_num/num_corr.py
@@ -1,11 +1,22 @@
 from scipy.fftpack import fft, fftshift
 from numpy import *
 
-def num_crl(filedir,filename):
-    "Add explanation here"
-    dat_nst=dat_nst.T
+def num_crl(wf_n):
+    """Function computes the autocorrelation function from given vectors
+    and the Discrete Fourier transform
 
+    Args:
+        wf_n(numpy array, complex): Wave function over time
+
+    Returns:
+        numpy array, complex: The autocorrelataion function over time.
+        numpy array, complex: The Discrete Fourier Transformation function over\
+        frequency
+    """
+    dat_nst=dat_nst.T
+    #setting up the time vector
     t2_wf=len(dat_nst[:,0])
+    
     t_wf=int(t2_wf/2)
     p_wf=len(dat_nst[0])
 
@@ -13,8 +24,10 @@ def num_crl(filedir,filename):
     for t in range(t_wf):
         comp_vc[t]=dat_nst[t*2] + dat_nst[1+t*2]*1j
 
-    ac_fc1=zeros([p_wf,t_wf+1],dtype=np.complex_)
+    ac_file=zeros([p_wf,t_wf+1],dtype=np.complex_)
     for n in range(p_wf):
-        ac_fc1[n]=np.sum(comp_vc[:,0]*np.conjugate(comp_vc[:,n]))
+        ac_file[n]=np.sum(comp_vc[:,0]*np.conjugate(comp_vc[:,n]))
 
-    return ac_fc1
+    FT_file = fft.fft(ac_file)
+    FT_t  = fft.fftfreq(len(ac_file))
+    return ac_file , FT_file , FT_t
