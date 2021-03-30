29b7dbf0dbf19380188e74e8ac9e7cbaaefddcaa
jagg01 <alexgg0197@gmail.com>
2021-03-30 17:39:03 +0200

    3rd try

--- src/mod_stat_num/test/test_read_thres.py
+++ src/mod_stat_num/test/test_read_thres.py
@@ -1,14 +1,15 @@
 import numpy as np
+import panda as pd
 import read_thres as thrs
 
 
-def chech_thr():
-    fil = build_df('./test/test_file.csv')
-    infmt = fil.read_df()
-    return infmt
+def chech_thr(name_p='./test/test_file.csv'):
+    file1 = pd.read_csv(name_p, r'\s+', header=None,skiprows=1)
+    fl_try = file1[0].values
+    return fl_try
 
 
-def test_thr(check_thr):
+def test_thr(fl_try):
     data, x = thrs.ths_def(check_thr, threshd=1.E-4)
     dat_nw = check_thr.drop(columns=["norm", "<x>", "<y>"])
     x_nw = dat_nw.columns.values
