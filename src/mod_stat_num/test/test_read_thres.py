824952bba7c1f53374d645c8fafbabba95e7ad0c
jagg01 <alexgg0197@gmail.com>
2021-03-30 18:12:11 +0200

    7th try

--- src/mod_stat_num/test/test_read_thres.py
+++ src/mod_stat_num/test/test_read_thres.py
@@ -9,7 +9,7 @@ def check_thr():
     return fl_try
 
 
-def test_thr(check_thr):
+def test_thr(fl_try):
     data, x = thrs.ths_def(check_thr, threshd=1.E-4)
     dat_nw = check_thr.drop(columns=["norm", "<x>", "<y>"])
     x_nw = dat_nw.columns.values
