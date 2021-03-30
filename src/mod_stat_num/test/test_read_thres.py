ac3a8372827512c3d49fec4bd7692bd11b7f7a3e
jagg01 <alexgg0197@gmail.com>
2021-03-30 17:40:39 +0200

    4th try

--- src/mod_stat_num/test/test_read_thres.py
+++ src/mod_stat_num/test/test_read_thres.py
@@ -3,14 +3,14 @@ import panda as pd
 import read_thres as thrs
 
 
-def chech_thr(name_p='./test/test_file.csv'):
+def check_thr(name_p='./test/test_file.csv'):
     file1 = pd.read_csv(name_p, r'\s+', header=None,skiprows=1)
     fl_try = file1[0].values
     return fl_try
 
 
 def test_thr(fl_try):
-    data, x = thrs.ths_def(check_thr, threshd=1.E-4)
+    data, x = thrs.ths_def(fl_try, threshd=1.E-4)
     dat_nw = check_thr.drop(columns=["norm", "<x>", "<y>"])
     x_nw = dat_nw.columns.values
     assert len(x) == len(x_nw)
