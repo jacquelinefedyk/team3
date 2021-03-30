df0b4931132b6474ba05e10ab1353c722f9c7c12
jagg01 <alexgg0197@gmail.com>
2021-03-30 17:20:33 +0200

    checking

--- src/mod_stat_num/test/test_read_thres.py
+++ src/mod_stat_num/test/test_read_thres.py
@@ -3,7 +3,7 @@ import read_thres as thrs
 
 
 def test_thr(check_thr):
-    data, x = thrs.ths_def(check_thr, threshd=1.E-5)
+    data, x = thrs.ths_def(check_thr, threshd=1.E-4)
     dat_nw = check_thr.drop(columns=["norm", "<x>", "<y>"])
     x_nw = dat_nw.columns.values
     assert len(x) == len(x_nw)
