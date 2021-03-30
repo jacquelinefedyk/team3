936cbe6af6111ca63a8a72976c67a606df7c3676
jagg01 <alexgg0197@gmail.com>
2021-03-30 17:30:27 +0200

    2nd try

--- src/mod_stat_num/test/test_read_thres.py
+++ src/mod_stat_num/test/test_read_thres.py
@@ -2,6 +2,12 @@ import numpy as np
 import read_thres as thrs
 
 
+def chech_thr():
+    fil = build_df('./test/test_file.csv')
+    infmt = fil.read_df()
+    return infmt
+
+
 def test_thr(check_thr):
     data, x = thrs.ths_def(check_thr, threshd=1.E-4)
     dat_nw = check_thr.drop(columns=["norm", "<x>", "<y>"])
